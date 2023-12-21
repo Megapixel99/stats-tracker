c82f350f4f286e6c7c76069fb02e532addfd10fb
Seth Wheeler <23089578+Megapixel99@users.noreply.github.com>
2023-12-21 13:33:12 -0600

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,7 @@
 const { tracker, dashboard } = require('../index.js');
 const { mongoUrl } = require('./env.js');
 
-const { WebSocketServer } = require('ws');
-
-let ws = tracker({
+let t = tracker({
   port: 3001,
   name: 'test',
 });
@@ -15,24 +13,24 @@ dashboard({
 })
 
 let inter = setInterval(function () {
-  if (ws.isConnected()) {
-    let j = ws.job.start('job');
+  if (t.isConnected()) {
+    let j = t.job.start('job');
 
     setTimeout(function () {
       j.stop();
     }, 10000);
 
-    ws.bytes.sent({
+    t.bytes.sent({
       'p': 'p',
     });
 
-    ws.bytes.received({
+    t.bytes.received({
       'p': 'p',
     });
 
-    ws.database.read(20);
+    t.database.read(20);
 
-    ws.database.written(25);
+    t.database.written(25);
 
     clearInterval(inter);
   }
