378381200075cf812a9989a023f7c742375598a6
Seth Wheeler <23089578+Megapixel99@users.noreply.github.com>
2025-02-16 21:37:07 -0600

    log

--- routers/webSocket.js
+++ routers/webSocket.js
@@ -54,6 +54,7 @@ class WS {
      } else {
         const searchCond = { server: this.name, date: dayStart };
         const stats = await models.stats.find(searchCond).lean();
+        console.log(stats);
 
         if (stats.length === 0) {
           await new models.stats(searchCond).save();
